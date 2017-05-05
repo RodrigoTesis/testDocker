866c6ed0bb87e90c2f218cedbc008c2a1c6032ea
Rodrigo Miranda <rodrigo.miranda@tsoftglobal.com>
2017-05-05 11:59:00 -0300

    Se agrega un nuevo mensaje

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo{
 	public static void main(String[] args){
 		System.out.println("Mi mensaje numero 1");
 		System.out.println("Mi mensaje numero 2");
+		System.out.println("Mi mensaje numero 3");
 	} 
 
 
